30b9f84f81c30831d862d6d545c62a2ebf459792
Steven Anderson <steven.a.anderson2012@gmail.com>
2022-03-07 21:04:25 -0700

    Add array slicing instead of fixed arrays

--- array_slice/array_slice_test.go
+++ array_slice/array_slice_test.go
@@ -6,31 +6,32 @@ import (
 )
 
 func TestSum(t *testing.T) {
-	assertArraySum := func(t testing.TB, got, want int, numbers [5]int) {
+	assertArraySum := func(t testing.TB, got, want int, numbers []int) {
 		t.Helper()
 		if got != want {
 			t.Errorf("\nGot: %d\nWant: %d\nNumbers: %v", got, want, numbers)
 		}
 	}
 
-	t.Run("Add all numbers in array together", func(t *testing.T){
-		numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("Array slice of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 
 		got := Sum(numbers)
-		want := 15
+		want := 6
+
 		assertArraySum(t, got, want, numbers)
 	})
 }
 
 func ExampleSum() {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 	sum := Sum(numbers)
 	fmt.Println(sum)
 	// Output: 15
 }
 
 func BenchmarkSum(b *testing.B) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3, 4, 5}
 
 	for i := 0; i < b.N; i++ {
 		Sum(numbers)
