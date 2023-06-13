e3dad403ae57d9cac7032712c822eb22966074e5
alerrian <steven.a.anderson2012@gmail.com>
2023-06-13 16:31:37 -0600

    update with beginning dictionary tests and initial search

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,1 +1,21 @@
 package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	assertStrings := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("\nGot: %q\nWant: %q", got, want)
+		}
+	}
+
+	t.Run("Known word", func(t *testing.T) {
+		dictionary := map[string]string{"test": "this is just a test"}
+
+		got := Search(dictionary, "test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+}
